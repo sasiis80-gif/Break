41f0496cfc49388af9d4aa5d53cc629f0901f50b
sasiis80-gif <sasini.somaratne80@gmail.com>
2026-04-21 19:27:46 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+#Take user input
+a = input ("Enter a word: ")
+#program to check keyword
+for i in a : #iterate the loop
+    if (i == "A"): #condition 1
+    #display the result
+       print ("A is found")
+       break #break statement
+    else:
+        #display the result
+        print ("A is not found")
